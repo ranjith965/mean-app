8a1667cb8ff25571ea4357590253a6b20343d2dc
ranjith965 <ranjith.kumar@juspay.in>
2018-05-07 17:48:35 +0530

    added sample routes

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
+app.get('/', function(req, res){
+   res
+     .status(200)
+     .sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/json', function(req, res){
+    res
+      .status(200)
+      .json({"jsonData":true});
+});
+
+app.get('/file', function(req, res){
+    res
+      .status(200)
+      .sendFile(path.join(__dirname, 'app.js'));
+});
 
 app.listen('3000',function(){
     console.log('server running at 3000');
