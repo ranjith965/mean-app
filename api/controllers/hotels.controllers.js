42b5bc277370f38a06d6295222c24c8de0d02ed7
ranjith965 <ranjith.kumar@juspay.in>
2018-05-15 16:19:45 +0530

    error trapping and hardening rest api

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -7,6 +7,15 @@ var runGeoQuery = function(req, res) {
   var lng = parseFloat(req.query.lng);
   var lat = parseFloat(req.query.lat);
 
+  if (isNaN(lng) || isNaN(lat)) {
+    res
+      .status(400)
+      .json({
+        "message" : "If supplied in querystring, lng and lat must both be numbers"
+      });
+    return;
+  }
+
   // A geoJSON point
   var point = {
     type : "Point",
@@ -23,11 +32,17 @@ var runGeoQuery = function(req, res) {
     .geoNear(point, geoOptions, function(err, results, stats) {
       console.log('Geo Results', results);
       console.log('Geo stats', stats);
-      res
-        .status(200)
-        .json(results);
+      if (err) {
+        console.log("Error finding hotels");
+        res
+          .status(500)
+          .json(err);
+      } else {
+        res
+          .status(200)
+          .json(results);
+      }
     });
-
 };
 
 module.exports.hotelsGetAll = function(req, res) {
@@ -37,13 +52,14 @@ module.exports.hotelsGetAll = function(req, res) {
 
   var offset = 0;
   var count = 5;
+  var maxCount = 50;
 
   if (req.query && req.query.lat && req.query.lng) {
     runGeoQuery(req, res);
     return;
   }
 
-  if (req.query && req.query.offset) {
+if (req.query && req.query.offset) {
     offset = parseInt(req.query.offset, 10);
   }
 
@@ -51,28 +67,71 @@ module.exports.hotelsGetAll = function(req, res) {
     count = parseInt(req.query.count, 10);
   }
 
+  if (isNaN(offset) || isNaN(count)) {
+    res
+      .status(400)
+      .json({
+        "message" : "If supplied in querystring, count and offset must both be numbers"
+      });
+    return;
+  }
+
+  if (count > maxCount) {
+    res
+      .status(400)
+      .json({
+        "message" : "Count limit of " + maxCount + " exceeded"
+      });
+    return;
+  }
+
   Hotel
     .find()
     .skip(offset)
     .limit(count)
     .exec(function(err, hotels) {
-      console.log("Found hotels", hotels.length);
-      res
-        .json(hotels);
+      console.log(err);
+      console.log(hotels);
+      if (err) {
+        console.log("Error finding hotels");
+        res
+          .status(500)
+          .json(err);
+      } else {
+        console.log("Found hotels", hotels.length);
+        res
+          .json(hotels);
+      }
     });
 
 };
 
 module.exports.hotelsGetOne = function(req, res) {
   var id = req.params.hotelId;
+
   console.log('GET hotelId', id);
 
   Hotel
     .findById(id)
     .exec(function(err, doc) {
+      var response = {
+        status : 200,
+        message : doc
+      };
+      if (err) {
+        console.log("Error finding hotel");
+        response.status = 500;
+        response.message = err;
+      } else if(!doc) {
+        console.log("HotelId not found in database", id);
+        response.status = 404;
+        response.message = {
+          "message" : "Hotel ID not found " + id
+        };
+      }
       res
-        .status(200)
-        .json(doc);
+        .status(response.status)
+        .json(response.message);
     });
 
 };
